51116760db29177cb4a2efa5d1c0f6d162f2664f
David Kamholz <lautgesetz@gmail.com>
2019-05-06 15:31:00 -0700

    initial implementation

---
+++ index.js
@@ -0,0 +1,39 @@
+let fs = require('fs');
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let RBT = require('icu-transliterator').RBT;
+registerCustomTransliterators();
+
+let app = express();
+
+app.use(bodyParser.text({ type: '*/*' }));
+
+app.post('/:id', transliterate(RBT.FORWARD));
+app.post('/:id/forward', transliterate(RBT.FORWARD));
+app.post('/:id/reverse', transliterate(RBT.REVERSE));
+
+app.listen(process.env.PORT || 3000);
+
+function registerCustomTransliterators() {
+  if (fs.existsSync('./transliterators.json')) {
+    let t = require('./transliterators.json');
+
+    t.forEach(function (obj) {
+      RBT.register(obj.name, obj.rules);
+    });
+  }
+}
+
+function transliterate(dir) {
+  return function(req, res) {
+    try {
+      let myRBT = RBT(req.params.id, dir);
+      res.set('Content-type', 'text/plain');
+      res.send(myRBT.transliterate(req.body));
+    } catch (e) {
+      console.error(e);
+      res.sendStatus(409);
+    }
+  }
+}
